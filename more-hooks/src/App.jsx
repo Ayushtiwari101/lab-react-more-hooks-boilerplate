44459d0e4cf7e6c0376de5026292ddcca573567d
Ayushtiwari101 <ayush.tiwari@kalvium.community>
2024-01-16 18:15:46 +0530

    added react

---
+++ more-hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import TaskList from './components/TaskList'
+import './App.css'
+import React, {Component} from 'react'
+
+function App() {
+
+  return (
+    <>
+      <TaskList/>
+    </>
+  )
+}
+
+export default App
